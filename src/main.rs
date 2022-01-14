811fbbdb967e31d2f69a8973046adbb412e71d4d
rigzba21 <rigbynined@gmail.com>
2022-01-14 13:51:01 -0500

    adding in fn to generate public key

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 
-use std::process::Command;
+use std::process::{Command, Stdio};
 use std::fs::File;
 use std::io::{Write};
 
@@ -18,19 +18,12 @@ struct Args {
     ip: String,
 }
 
-fn main() {
-    let args = Args::parse();
-    println!("Setting Wireguard Server to: {}", args.ip);
-
-    generate_private_key();
-}
-
 /// Generate the WireGuard Server Private Key
 fn generate_private_key() {
     let wg_genkey_status = wg_genkey();
     match wg_genkey_status {
         Ok(private_key) => {
-            write_private_key_file(private_key).unwrap();
+            write_key_file(private_key, String::from("server_private_key")).unwrap();
         }
         _ => {
             println!("{:#?}", WgInitErrors::FailedToGenPrivateKey);
@@ -39,9 +32,9 @@ fn generate_private_key() {
 }
 
 /// Write our private key to a file for basic persistence 
-fn write_private_key_file(key: String) -> std::io::Result<()> {
-    println!("{:#?}", key);
-    let mut file = File::create("server_private_key")?;
+fn write_key_file(key: String, file: String) -> std::io::Result<()> {
+    //println!("{:#?}", key);
+    let mut file = File::create(file)?;
     file.write_all(key.as_bytes())?;
     Ok(())
 }
@@ -58,6 +51,7 @@ fn wg_genkey() -> Result<String, WgInitErrors> {
     match status_code {
         Some(0) => {
             let wg_private_key = String::from_utf8(output.stdout).unwrap();
+            println!("PRIVATE KEY: {}", wg_private_key);
             return Ok(wg_private_key);
         }
         _ => {
@@ -69,4 +63,53 @@ fn wg_genkey() -> Result<String, WgInitErrors> {
 
 }
 
+/// Execute the wg pubkey command to return the server public key
+fn wg_pubkey() -> Result<String, WgInitErrors>{
+    let _cat_private_key = Command::new("cat")
+        .arg("server_private_key")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to cat server_private_key");
 
+    let output = Command::new("wg")
+        .arg("pubkey")
+        .stdin(_cat_private_key.stdout.unwrap())
+        .output()
+        .expect("failed to execute command \"wg pubkey\"");
+
+    let status_code = output.status.code();
+
+    match status_code {
+        Some(0) => {
+            let wg_public_key = String::from_utf8(output.stdout).unwrap();
+            println!("PUBLIC KEY: {}", wg_public_key);
+            return Ok(wg_public_key);
+        }
+        _ => {
+            let std_err = String::from_utf8(output.stderr).unwrap();
+            println!("{:#?}", std_err);
+            return Err(WgInitErrors::FailedToGenPrivateKey)
+        }
+    }
+}
+
+/// Generate the WireGuard Server Public Key
+fn generate_public_key() {
+    let wg_pubkey_status = wg_pubkey();
+    match wg_pubkey_status {
+        Ok(public_key) => {
+            write_key_file(public_key, String::from("server_public_key")).unwrap();
+        }
+        _ => {
+            println!("{:#?}", WgInitErrors::FailedToGenPrivateKey);
+        }
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("Setting Wireguard Server to: {}", args.ip);
+
+    generate_private_key();
+    generate_public_key();
+}
