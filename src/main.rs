c380a8a08e5567c0908c94f84f1f943b9f75a089
rigzba21 <rigbynined@gmail.com>
2022-01-18 13:30:55 -0500

    bumping version to 0.0.2

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ enum RoadGuardAction {
 }
 
 #[derive(Parser, Debug)]
-#[clap(author = "Jon V [rigzba21]", version = "0.0.1", about = "roadguard - Setup a Road Warrior Style Wireguard VPN", long_about = None)]
+#[clap(author = "Jon V [rigzba21]", version = "0.0.2", about = "roadguard - Setup a Road Warrior Style Wireguard VPN", long_about = None)]
 struct Args {
     /// IP Address to set the Wireguard Server
     #[clap(short, long, default_value = "10.253.3.1")]
